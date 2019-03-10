34b69c82593d2178672233aaff2e50eaa944e40b
TonyXia2001 <summertony717@gmail.com>
2019-03-10 13:19:19 -0400

    l

--- traveland/js/googledirectionapi.js
+++ traveland/js/googledirectionapi.js
@@ -1,13 +1,13 @@
-// var xmlrequestTony = new XMLHttpRequest();
-// var URLTony = "http://cors.io/?https://maps.googleapis.com/maps/api/directions/json?origin=Disneyland&destination=Universal+Studios+Hollywood&key=AIzaSyA4ffefqz2hpAaDJECnpiS3EbFLr0hF16g";
-// var routeTony;
-//
-// xmlrequestTony.onreadystatechange = function() {
-//     if (this.readyState == 4 && this.status == 200) {
-//         console.log("haha");
-//         routeTony = JSON.parse(this.responseText);
-//         console.log(routeTony);
-//     }
-// };
-// xmlrequestTony.open("GET", URLTony, true);
-// xmlrequestTony.send();
+var xmlrequestTony = new XMLHttpRequest();
+var URLTony = "https://crossorigin.me/http://cors.io/?https://maps.googleapis.com/maps/api/directions/json?origin=Disneyland&destination=Universal+Studios+Hollywood&key=AIzaSyA4ffefqz2hpAaDJECnpiS3EbFLr0hF16g";
+var routeTony;
+
+xmlrequestTony.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        console.log("haha");
+        routeTony = JSON.parse(this.responseText);
+        console.log(routeTony);
+    }
+};
+xmlrequestTony.open("GET", URLTony, true);
+xmlrequestTony.send();
