548f0ba83178ee0fb4d5e863b41881f21f4c1481
TonyXia2001 <summertony717@gmail.com>
2019-03-10 14:19:28 -0400

    d

--- traveland/js/googledirectionapi.js
+++ traveland/js/googledirectionapi.js
@@ -1,6 +1,7 @@
 var mapTony;
 
 function initMapTony() {
+  console.log("yeet");
   mapTony = new google.maps.Map(document.getElementById("mapTony"), {
     center: {lat: 33, lng: 150},
     zoom: 8
