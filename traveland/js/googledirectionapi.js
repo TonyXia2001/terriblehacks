dbc0d10d8252b2a513ef2b4095de318b3ac9f3f1
TonyXia2001 <summertony717@gmail.com>
2019-03-10 16:12:08 -0400

    d

--- traveland/js/googledirectionapi.js
+++ traveland/js/googledirectionapi.js
@@ -3,9 +3,30 @@ var mapTony;
 function initMapTony() {
   console.log("yeet");
   mapTony = new google.maps.Map(document.getElementById("mapTony"), {
-    center: {lat: 40, lng: 120},
+    center: {lat: 43.4, lng: -80.5},
     zoom: 8
   });
 
-  
+  tripRoute = {
+    origin:"Chicago, IL",
+    destination:"Los Angeles, CA",
+    provideRouteAlternatives: false,
+    travelMode: 'DRIVING',
+    unitSystem: google.maps.UnitSystem.IMPERIAL
+  };
+
+  var directionsService = new google.maps.DirectionsService;
+  var directionsDisplay = new google.maps.DirectionsRenderer;
+  directionsDisplay.setMap(mapTony);
+  calculateAndDisplayRoute(directionsService, directionsDisplay)
+  function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+    directionsService.route(tripRoute, function(response, status) {
+      if (status === 'OK') {
+        directionsDisplay.setDirections(response);
+      } else {
+        window.alert('Directions request failed due to ' + status);
+      }
+    });
+  }
+
 }
